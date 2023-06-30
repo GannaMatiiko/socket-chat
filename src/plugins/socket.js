a0bb21bf3eb88e8f36c78a507e83f28696df295f
Anna <gannamatiyko@gmail.com>
2023-07-01 00:13:22 +0300

    update socket config file

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,7 +1,19 @@
 import { io } from "socket.io-client";
 
 const URL = "http://localhost:4000";
-const socket = io(URL, { autoConnect: false });
+console.log(localStorage.getItem('token'));
+const socket = io(URL, {
+    // autoConnect: false,
+transportOptions: {
+polling: {
+ extraHeaders: {
+   //todo тут реальный токен юзера подставить
+   //todo во второй итерации (когда тут заработает) Authorization заменить на authToken и слать сразу токен без Bearer и пробела
+   'Authorization': `Bearer ${localStorage.getItem('token')}`,
+ },
+},
+},
+});
 
 socket.onAny((event, ...args) => {
     console.log('*', event, args, 'socketId', socket.id);
