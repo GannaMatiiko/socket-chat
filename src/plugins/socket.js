c3e2c3ed79ae88c4de9475c7962b1f669c8cf3ab
Anna <gannamatiyko@gmail.com>
2023-07-06 22:43:17 +0300

    update view on recieving socket message

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
+import store from '@/store/index.js';
 
 const URL = "http://localhost:4000";
 const socket = io(URL, {
@@ -10,6 +11,18 @@ polling: {
  },
 },
 },
+});
+
+// Update view on recieving message by socket
+socket.on("messagePublished", (msg) => {
+  console.log('on message %%', msg);
+  store.dispatch('appendMessage', {
+      login: msg.login,
+      text: msg.message,
+      createdAt: msg.createdAt,
+      isAuthorOwner: msg.isAuthorOwner,
+      isServiceMessage: msg.isServiceMessage,
+  });
 });
 
 socket.onAny((event, ...args) => {
